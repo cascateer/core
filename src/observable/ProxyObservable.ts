3117c3afb5a739c432fa8b9bd657f72298473b31
cascateer <cascateer@gmail.com>
2026-04-26 13:09:04 +0200

    refcount

--- src/observable/ProxyObservable.ts
+++ src/observable/ProxyObservable.ts
@@ -1,5 +1,14 @@
-import { thru } from "lodash";
-import { isObservable, Observable, of } from "rxjs";
+import { tap, thru } from "lodash";
+import {
+  isObservable,
+  map,
+  Observable,
+  of,
+  scan,
+  Subject,
+  Subscriber,
+  UnaryFunction,
+} from "rxjs";
 
 export interface ProxyObservableDescriptor<T, U> {
   (target: T):
@@ -16,6 +25,7 @@ export class ProxyObservable<
   T extends Observable<X> = Observable<X>,
 > extends Observable<Y> {
   pending: Observable<boolean>;
+  refCount: Observable<number>;
 
   constructor(
     target: T,
@@ -27,8 +37,22 @@ export class ProxyObservable<
         isObservable(descriptor) ? { value: descriptor } : descriptor,
     );
 
-    super((subscriber) => value.subscribe(subscriber));
+    const subscribers = new Subject<UnaryFunction<Set<Subscriber<Y>>, void>>();
+
+    super((subscriber) => {
+      subscribers.next((subscribers) => subscribers.add(subscriber));
+
+      subscriber.add(() =>
+        subscribers.next((subscribers) => subscribers.delete(subscriber)),
+      );
+
+      return value.subscribe(subscriber);
+    });
 
     this.pending = pending;
+    this.refCount = subscribers.pipe(
+      scan(tap, new Set<Subscriber<Y>>()),
+      map((subscribers) => subscribers.size),
+    );
   }
 }
