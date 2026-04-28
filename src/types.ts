72631fc574371fae102491a30150500926e66678
cascateer <cascateer@gmail.com>
2026-04-28 21:03:28 +0200

    no memo?

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,6 @@ import { Dictionary, mapValues, tap } from "lodash";
 import { combineLatest, ReplaySubject, switchMap, UnaryFunction } from "rxjs";
 import { Observable } from "rxjs/internal/Observable";
 import { ObservableInput } from "rxjs/internal/types";
-import { memoizeHashed } from "./lib/memoizeHashed";
 import { ProxyObservable } from "./observable";
 import { concat, every, some } from "./operators";
 
@@ -35,15 +34,15 @@ export class ProxyEffectInterceptor extends ReplaySubject<
   intercept<Effects extends Dictionary<ProxyEffect<any, any>>>(
     effects: Effects,
   ): ProxyEffects<Effects> {
-    return mapValues(effects, (effect) =>
-      memoizeHashed((args) =>
+    return mapValues(
+      effects,
+      (effect) => (args) =>
         tap(
           new ProxyObservable(effect(args), (target, receiver) =>
             combineLatest([target.pending, receiver.refCount]).pipe(every()),
           ),
           (source) => this.next(source),
         ),
-      ),
     );
   }
 
