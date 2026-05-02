28441dedfb848396fc241d40f4c61ff5ff61ce22
cascateer <cascateer@gmail.com>
2026-05-02 12:02:16 +0200

    shareReplay x2

--- src/multicast.ts
+++ src/multicast.ts
@@ -8,7 +8,7 @@ import {
   mergeMap,
   Observable,
   scan,
-  share,
+  shareReplay,
 } from "rxjs";
 import { v4 } from "uuid";
 import { property } from "./lib";
@@ -74,7 +74,7 @@ const actions = proxyReplaySubject<Observable<InMessages>, OutMessages>(
           ),
         ),
       ),
-      share(),
+      shareReplay(),
     ),
 );
 
@@ -91,6 +91,7 @@ self.addEventListener("connect", ({ ports }) => {
             : { ...action, previousId: previousAction!.id },
         ),
         exchangeWith<MulticastClientMessage, MulticastActionMessage<any>>(port),
+        shareReplay(),
         map((message) => ({ ...message, origin: port })),
         concat(),
         flatMap((messages) =>
