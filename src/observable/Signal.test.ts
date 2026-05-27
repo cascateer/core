b41fb063fa093932a542f6183d631573586120e1
cascateer <cascateer@gmail.com>
2026-05-27 13:41:54 +0200

    new test

--- src/observable/Signal.test.ts
+++ src/observable/Signal.test.ts
@@ -1,12 +1,40 @@
-import { lastValueFrom, of, toArray } from "rxjs";
+import { identity } from "lodash";
+import {
+  lastValueFrom,
+  of,
+  ReplaySubject,
+  scan,
+  startWith,
+  toArray,
+} from "rxjs";
 import { expect, test } from "vitest";
+import { Transform } from "../types";
 import { ComputedSignal } from "./Signal";
 
-test("projects", () =>
-  lastValueFrom(
-    new ComputedSignal({
-      value: of({ number: 1 }, { number: 2 }, { number: 3 }),
-    })
-      .property("number")
-      .pipe(toArray()),
-  ).then((numbers) => expect(numbers).toEqual([1, 2, 3])));
+test("projection", () => {
+  const signal = new ComputedSignal({
+    value: of({ number: 1 }, { number: 2 }, { number: 3 }),
+  }).property("number");
+
+  lastValueFrom(signal.pipe(toArray())).then((numbers) =>
+    expect(numbers).toEqual([1, 2, 3]),
+  );
+});
+
+test("transformation", () => {
+  const transforms = new ReplaySubject<Transform<any>>();
+  const signal = new ComputedSignal({
+    value: transforms.pipe(
+      startWith(identity),
+      scan((state, transform) => transform(state), { number: 1 }),
+    ),
+  }).property("number");
+
+  transforms.next(signal.reflector.predicate((number) => number + 1));
+  transforms.next(signal.reflector.predicate((number) => number + 2));
+  transforms.complete();
+
+  return lastValueFrom(signal.pipe(toArray())).then((numbers) =>
+    expect(numbers).toEqual([1, 2, 4]),
+  );
+});
