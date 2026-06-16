33cdccc0afcefb141100f1d63292244af7544840
cascateer <cascateer@gmail.com>
2026-06-16 14:26:43 +0200

    rollback

--- src/terminal.ts
+++ src/terminal.ts
@@ -115,7 +115,6 @@ export class ExtendableTerminalAdapter<
                 actions: StoreActions;
               };
               api: {
-                effects: ApiEffects;
                 actions: ApiActions;
               };
               terminal: {
@@ -143,7 +142,6 @@ export class ExtendableTerminalAdapter<
                   actions: this.context.store.actions,
                 },
                 api: {
-                  effects: this.context.api.effects,
                   actions: this.context.api.actions,
                 },
                 terminal: {
