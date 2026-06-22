2890f19aaae106b74fdd9a6dea8a141e31737fd0
cascateer <cascateer@gmail.com>
2026-06-22 07:52:18 +0200

    debugging api cashing

--- src/lib/memoize.ts
+++ src/lib/memoize.ts
@@ -2,4 +2,6 @@ import * as lodash from "lodash";
 import objectHash from "object-hash";
 
 export const memoize = <T extends (...args: any) => any>(func: T) =>
-  lodash.memoize(func, (...args: Parameters<T>) => objectHash(args ?? null));
+  lodash.memoize(func, (...args: Parameters<T>) =>
+    lodash.tap(objectHash(args ?? null), (hash) => console.log({ hash, args })),
+  );
