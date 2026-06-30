4aa5e383b204443389180b73a8dc0463ca99e91e
cascateer <cascateer@gmail.com>
2026-06-30 21:41:25 +0200

    eslint package

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,4 @@
-import eslint from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import config from "@cascateer/eslint";
+import { defineConfig } from "eslint/config";
 
-export default tseslint.config(
-  { ignores: ["dist"] },
-  {
-    extends: [
-      eslint.configs.recommended,
-      ...tseslint.configs.recommendedTypeChecked,
-      ...tseslint.configs.stylistic,
-      {
-        languageOptions: {
-          parserOptions: {
-            projectService: true,
-            tsconfigRootDir: import.meta.dirname,
-          },
-        },
-      },
-    ],
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    rules: {
-      // https://eslint.org/docs/latest/rules/
-      // https://typescript-eslint.io/rules/
-      "@typescript-eslint/no-unnecessary-condition": "warn",
-      "@typescript-eslint/strict-boolean-expressions": "warn",
-      "no-useless-rename": "warn",
-    },
-  },
-);
+export default defineConfig(config);
